1917f5fc91b62eb694e0f274592cf9195a264d2e
Atoshsingh <toshkumrt@gmail.com>
2024-09-18 14:51:46 +0530

    Redux learning first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Counter from "./components/Counter"
+
+function App() {
+
+  return (
+    <>
+    <Counter/>
+    </>
+  )
+}
+export default App
+
+//npx create-vite my-project-name --tamplate react; 
+//installs = npm install @reduxjs/toolkit react-redux
